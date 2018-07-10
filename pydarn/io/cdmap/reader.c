664c49560e3b678fa6300b13e082efb6f7657462
mts299 <mts299888@gmail.com>
2018-07-10 15:31:06 -0600

    finished reading in the vector arrays and cleaned it up from dmapio.c, next will be to test it

--- pydarn/io/cdmap/reader.c
+++ pydarn/io/cdmap/reader.c
@@ -37,9 +37,8 @@ read_dmap_rec(PyObject *self, PyObject *args)
      *
      *
      */
-    PyObject *fitacf_record = PyList_New(); 
     PyObject *beam_record = PyDict_New();
-    
+    int nrange = 0;
 
     fp = fdopen(fd,"r"); // fp - file pointer
     if ( fp == NULL)
@@ -65,49 +64,152 @@ read_dmap_rec(PyObject *self, PyObject *args)
         PyObject *value;
         
 
-        if (s->type == DATASHORT)
-        {    
-            value = Py_BuildValue(("i",*(s->data.sptr));   
-        }    
-        else if (s->type == DATACHAR)
-        {    
-            value = Py_BuildValue("c"s->data.cptr);
-        }
-        else if (s->type == DATAINT)
-        {    
-            value = Py_BuildValue("i",*(s->data.iptr))
-        }
-        else if (s->type == DATAFLOAT)
-        {    
-            value = Py_BuildValue("f",*(s->data.fptr))
-        }
-        else if (s->tyoe == DATAFLOAT)
-        {    
-            value = Py_BuildValue("d",*(s->data.dptr))
+        if (scalardata->type == DATASHORT)
+        {   
+            if ( strcmp(scalardata->name, "nrang") == 0 )
+            {
+                nrange = scalaradata->data.sptr; // need this value for the vector arrays
             }
-        else if (s->type == DATASTRING)
+            value = Py_BuildValue(("i",*(scalardata->data.sptr));   
+        }    
+        else if (scalardata->type == DATACHAR)
+        {    
+            value = Py_BuildValue("c"scalardata->data.cptr);
+        }
+        else if (scalardata->type == DATAINT)
+        {    
+            value = Py_BuildValue("i",*(scalardata->data.iptr))
+        }
+        else if (scalardata->type == DATAFLOAT)
+        {    
+            value = Py_BuildValue("f",*(scalardata->data.fptr))
+        }
+        else if (scalardata->tyoe == DATAFLOAT)
+        {    
+            value = Py_BuildValue("d",*(scalardata->data.dptr))
+            }
+        else if (scalardata->type == DATASTRING)
         {
-            value = Py_BuildValue("s",*((char **)s->data.vptr))
+            value = Py_BuildValue("s",*((char **)scalardata->data.vptr))
         }
         else
         {
             Py_DECREF(value);
-            Py_DECREF(fitacf_record);
             Py_DECREF(beam_record);
             return -1;
         }
         
-        if ( PyDict_SetItemString(beamData,s->name,value) < 0 )
+        if ( PyDict_SetItemString(beam_record,scalardata->name,value) < 0 )
         {
             Py_DECREF(value);
-            Py_DECREF(fitacf_record);
             Py_DECREF(beam_record);
             return -1;
 
         }
         PyCLEAR(value)
     }
-    return 0;
+
+    // Parse the vectors now
+    for(int i=0; i < beamdata->anum;i++)
+    {
+        arraydata = deamdata->arr[i];
+        PyObject *vector_list = PyListNew(0);
+
+        // special case 
+        if ( arraydata->dim <= 2 )
+        {
+            if (strcmp(arraydata->name,"ltab") == 0 && arraydata->type == DATASHORT)
+            {
+                for (int j=0; j < arraydata->rng[1]-1; j++)
+                {
+                    value = Py_BuildValue("[i,i]", arraydata->data.sptr[j+2], arraydata->data.sptr[j*2+1]);
+                    PyList_Append(vector_list,value);
+                    PyCLEAR(value);
+                }
+            }
+            else
+            {
+                for(int j=0; j < arraydata->rng[0]; j++)
+                {
+                    if(arraydata->type == DATASHORT)
+                    {
+                        value = Py_BuildValue("i", arraydata->data.sptr[j]);
+                    }
+                    else if (scalardata->type == DATACHAR)
+                    {    
+                        value = Py_BuildValue("c"scalardata->data.cptr);
+                    }
+                    else if (scalardata->type == DATAINT)
+                    {    
+                        value = Py_BuildValue("i",*(scalardata->data.iptr))
+                    }
+                    else if (scalardata->type == DATAFLOAT)
+                    {    
+                        value = Py_BuildValue("f",*(scalardata->data.fptr))
+                    }
+                    else if (scalardata->tyoe == DATAFLOAT)
+                    {    
+                        value = Py_BuildValue("d",*(scalardata->data.dptr))
+                        }
+                    else if (scalardata->type == DATASTRING)
+                    {
+                        value = Py_BuildValue("s",data->data.vptr))
+                    }
+                    else
+                    {
+                                Py_DECREF(value);
+                                Py_DECREF(vector_list);
+                                Py_DECREF(beam_record);
+                                return -1;
+
+                    }
+        }
+        else if (arraydata->dim ==  3) // parses acfd and xcfd
+        {
+            for(int j=0; j < nrange; j++)
+            {
+                for(int k=0; k < arraydata->rng[1]; k++)
+                {
+                    for (int m=0; m < 2; m++)
+                    {
+                            if(arraydata->type == DATAFlOAT)
+                            {
+                                value = Py_BuildValue("f",arraydata->fptr[(j*arraydata->rng[1]+k)*2+m]);
+                            }
+                            else
+                            {
+                                Py_DECREF(value);
+                                Py_DECREF(vector_list);
+                                Py_DECREF(beam_record);
+                                return -1;
+
+                            }
+                            PyList_Append(vector_list, value);
+                            PyClear(value);
+
+
+                    }
+                }
+            }
+
+        }
+        if ( PyDict_SetItemString(beam_record,arraydata->name,vector_list) < 0 )
+        {
+            Py_DECREF(value);
+            Py_DECREF(beam_record);
+            Py_DECREF(vector_list);
+            return -1;
+
+        }
+        PyCLEAR(value);
+        PyCLEAR(vector_list);
+
+
+    }
+    Py_DECREF(value);
+    Py_DECREF(vector_list);
+   
+    return beam_record;
 }
 
 
